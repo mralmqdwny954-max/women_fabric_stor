67b0e636d0d5265800aac57a2c243920b74751d5
salman <mralmqdwny954@gmail.com>
2026-09-09 00:07:08 +0300

    Measure image upload time

--- routers/admin.py
+++ routers/admin.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import joinedload
 from passlib.context import CryptContext
 from sqlalchemy.exc import IntegrityError
 from utils.upload import save_image, delete_image
+import time
 
 router = APIRouter(
     prefix="/admin",
@@ -620,10 +621,14 @@ def add_product(
 
         # حفظ الصورة
 
+        start_upload = time.time()
+
         image_path = save_image(
-            image,
-            "products"
+             image,
+             "products"
         )
+
+        print("IMAGE UPLOAD TIME:", time.time() - start_upload)
 
         new_product = Product(
 
